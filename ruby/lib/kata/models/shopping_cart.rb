2eff2ad712d2b4624af46333d8174e0da5fdfbdc
Vahdet Pervan <vpervan22@gmail.com>
2025-01-15 00:30:09 +0100

    Renamed number_of_x to something more descriptive.

--- ruby/lib/kata/models/shopping_cart.rb
+++ ruby/lib/kata/models/shopping_cart.rb
@@ -51,16 +51,16 @@ class Kata::ShoppingCart
         if offer.offer_type == Kata::SpecialOfferType::FIVE_FOR_AMOUNT
           offer_quantity = 5
         end
-        number_of_x = quantity_as_int / offer_quantity
+        item_units_for_discount = quantity_as_int / offer_quantity
         if offer.offer_type == Kata::SpecialOfferType::THREE_FOR_TWO && quantity_as_int > 2
-          discount_amount = quantity * unit_price - ((number_of_x * 2 * unit_price) + quantity_as_int % 3 * unit_price)
+          discount_amount = quantity * unit_price - ((item_units_for_discount * 2 * unit_price) + quantity_as_int % 3 * unit_price)
           discount = Kata::Discount.new(product, "3 for 2", discount_amount)
         end
         if offer.offer_type == Kata::SpecialOfferType::TEN_PERCENT_DISCOUNT
           discount = Kata::Discount.new(product, offer.argument.to_s + "% off", quantity * unit_price * offer.argument / 100.0)
         end
         if offer.offer_type == Kata::SpecialOfferType::FIVE_FOR_AMOUNT && quantity_as_int >= 5
-          discount_total = unit_price * quantity - (offer.argument * number_of_x + quantity_as_int % 5 * unit_price)
+          discount_total = unit_price * quantity - (offer.argument * item_units_for_discount + quantity_as_int % 5 * unit_price)
           discount = Kata::Discount.new(product, offer_quantity.to_s + " for " + offer.argument.to_s, discount_total)
         end
 
