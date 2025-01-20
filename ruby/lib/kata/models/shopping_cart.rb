7748c0049643437229d5c26abcbe504ffdd584c1
Vahdet Pervan <vpervan22@gmail.com>
2025-01-20 23:58:22 +0100

    Bring offer_quantity variable closer to the place it gets used.

--- ruby/lib/kata/models/shopping_cart.rb
+++ ruby/lib/kata/models/shopping_cart.rb
@@ -35,13 +35,13 @@ class Kata::ShoppingCart
         unit_price = catalog.unit_price(product)
         quantity_as_int = quantity.to_i
         offer_quantity = 1
-        offer_quantity = 3 if offer.offer_type == Kata::SpecialOfferType::THREE_FOR_TWO
         if offer.offer_type == Kata::SpecialOfferType::TWO_FOR_AMOUNT && quantity_as_int >= 2
           total = offer.argument * (quantity_as_int / 2) + quantity_as_int % 2 * unit_price
           discount_n = unit_price * quantity - total
           discount = Kata::Discount.new(product, "2 for " + offer.argument.to_s, discount_n)
         end
         offer_quantity = 5 if offer.offer_type == Kata::SpecialOfferType::FIVE_FOR_AMOUNT
+        offer_quantity = 3 if offer.offer_type == Kata::SpecialOfferType::THREE_FOR_TWO
         item_units_for_discount = quantity_as_int / offer_quantity
         if offer.offer_type == Kata::SpecialOfferType::THREE_FOR_TWO && quantity_as_int > 2
           discount_amount = quantity * unit_price - ((item_units_for_discount * 2 * unit_price) + quantity_as_int % 3 * unit_price)
