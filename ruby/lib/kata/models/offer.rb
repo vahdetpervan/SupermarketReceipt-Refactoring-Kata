38c9cb88fbd3abf253a67b19e8ba9fde18980335
Vahdet Pervan <vpervan22@gmail.com>
2025-03-03 23:45:52 +0100

    Need to replace assignment with return.

--- ruby/lib/kata/models/offer.rb
+++ ruby/lib/kata/models/offer.rb
@@ -12,7 +12,12 @@ class Kata::Offer
     if self.offer_type == Kata::SpecialOfferType::TWO_FOR_AMOUNT
       total = self.argument * (quantity.to_i / 2) + quantity.to_i % 2 * unit_price
       discount_n = unit_price * quantity - total
-      discount = Kata::Discount.new(self.product, "2 for " + self.argument.to_s, discount_n)
+      return Kata::Discount.new(self.product, "2 for " + self.argument.to_s, discount_n)
+    end
+    if self.offer_type == Kata::SpecialOfferType::FIVE_FOR_AMOUNT && quantity.to_i >= 5
+      item_units_for_discount = quantity.to_i / 5
+      discount_total = unit_price * quantity - (argument * item_units_for_discount + quantity.to_i % 5 * unit_price)
+      return Kata::Discount.new(product, 5.to_s + " for " + argument.to_s, discount_total)
     end
   end
 
