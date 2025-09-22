8516cd88852228080f41827d692367821eb93234
Vahdet Pervan <vpervan22@gmail.com>
2025-09-22 12:07:39 +0200

    No need to insert variable with named arguments.

--- ruby/lib/kata/models/offer.rb
+++ ruby/lib/kata/models/offer.rb
@@ -8,9 +8,9 @@ class Kata::Offer
   end
 
   def discount(quantity:, unit_price:)
-    return Kata::TwoForAmount.new(argument: argument, quantity: quantity, unit_price: unit_price, product: product).build if offer_type == Kata::SpecialOfferType::TWO_FOR_AMOUNT
-    return Kata::ThreeForTwo.new(quantity: quantity, unit_price: unit_price, product: product).build if offer_type == Kata::SpecialOfferType::THREE_FOR_TWO
-    return Kata::TenPercentDiscount.new(product: product, argument: argument, quantity: quantity, unit_price: unit_price).build if offer_type == Kata::SpecialOfferType::TEN_PERCENT_DISCOUNT
-    Kata::FiveForAmount.new(quantity: quantity, unit_price: unit_price, argument: argument, product: product).build if offer_type == Kata::SpecialOfferType::FIVE_FOR_AMOUNT && quantity.to_i >= 5
+    return Kata::TwoForAmount.new(argument:, quantity:, unit_price:, product:).build if offer_type == Kata::SpecialOfferType::TWO_FOR_AMOUNT
+    return Kata::ThreeForTwo.new(quantity:, unit_price:, product:).build if offer_type == Kata::SpecialOfferType::THREE_FOR_TWO
+    return Kata::TenPercentDiscount.new(product:, argument:, quantity:, unit_price:).build if offer_type == Kata::SpecialOfferType::TEN_PERCENT_DISCOUNT
+    Kata::FiveForAmount.new(quantity:, unit_price:, argument:, product:).build if offer_type == Kata::SpecialOfferType::FIVE_FOR_AMOUNT && quantity.to_i >= 5
   end
 end
