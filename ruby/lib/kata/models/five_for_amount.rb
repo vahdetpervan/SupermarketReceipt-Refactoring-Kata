b78e68796a00f2c4a068957ac9db41500fb9fb0a
Vahdet Pervan <vpervan22@gmail.com>
2025-09-22 12:05:17 +0200

    Extract five for amount logic into its own class.

---
+++ ruby/lib/kata/models/five_for_amount.rb
@@ -0,0 +1,14 @@
+class Kata::FiveForAmount
+  def initialize(quantity:, unit_price:, argument:, product:)
+    @quantity = quantity
+    @unit_price = unit_price
+    @argument = argument
+    @product = product
+  end
+
+  def build
+    item_units_for_discount = @quantity.to_i / 5
+    discount_total = @unit_price * @quantity - (@argument * item_units_for_discount + @quantity.to_i % 5 * @unit_price)
+    Kata::Discount.new(@product, 5.to_s + " for " + @argument.to_s, discount_total)
+  end
+end
--- ruby/lib/kata/models/offer.rb
+++ ruby/lib/kata/models/offer.rb
@@ -12,12 +12,7 @@ class Kata::Offer
     return Kata::TwoForAmount.new(argument: argument, quantity: quantity, unit_price: unit_price, product: product).build if offer_type == Kata::SpecialOfferType::TWO_FOR_AMOUNT
     return Kata::ThreeForTwo.new(quantity: quantity, unit_price: unit_price, product: product).build if offer_type == Kata::SpecialOfferType::THREE_FOR_TWO
     return Kata::TenPercentDiscount.new(product: product, argument: argument, quantity: quantity, unit_price: unit_price).build if offer_type == Kata::SpecialOfferType::TEN_PERCENT_DISCOUNT
-
-    if offer_type == Kata::SpecialOfferType::FIVE_FOR_AMOUNT && quantity.to_i >= 5
-      item_units_for_discount = quantity.to_i / 5
-      discount_total = unit_price * quantity - (argument * item_units_for_discount + quantity.to_i % 5 * unit_price)
-      Kata::Discount.new(product, 5.to_s + " for " + argument.to_s, discount_total)
-    end
+    Kata::FiveForAmount.new(quantity: quantity, unit_price: unit_price, argument: argument, product: product).build if offer_type == Kata::SpecialOfferType::FIVE_FOR_AMOUNT && quantity.to_i >= 5
   end
 
 end
