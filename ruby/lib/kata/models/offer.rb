b8b12a60524dc426181fef6c184d66061d475f15
Vahdet Pervan <vpervan22@gmail.com>
2025-09-27 22:19:58 +0200

    Remove old logic for five for amount discount.

--- ruby/lib/kata/models/offer.rb
+++ ruby/lib/kata/models/offer.rb
@@ -20,10 +20,6 @@ class Kata::Offer
     end
     if offer_type == Kata::SpecialOfferType::FIVE_FOR_AMOUNT && quantity.to_i >= 5
       return calculate_five_for_amount_discount(quantity:, unit_price:)
-
-      item_units_for_discount = quantity.to_i / 5
-      discount_total = unit_price * quantity - (argument * item_units_for_discount + quantity.to_i % 5 * unit_price)
-      Kata::Discount.new(product, 5.to_s + " for " + argument.to_s, discount_total)
     end
   end
 
