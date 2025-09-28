2772ef9a3dd74d5cb0bb8046877d9ddf3a244ccb
Vahdet Pervan <vpervan22@gmail.com>
2025-09-28 23:05:25 +0200

    Constructor setup for two for amount discount class.

--- ruby/lib/kata/models/discounts/two_for_amount.rb
+++ ruby/lib/kata/models/discounts/two_for_amount.rb
@@ -1,7 +1,12 @@
 module Kata::Discounts
   class TwoForAmount
-    def initialize
+    attr_reader :amount, :quantity, :unit_price, :product
 
+    def initialize(argument:, quantity:, unit_price:, product:)
+      @argument = argument
+      @quantity = quantity
+      @unit_price = unit_price
+      @product = product
     end
 
     def calculate
