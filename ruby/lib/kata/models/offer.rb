04545470b37c66a77716b0741bc21e2dfcd99495
Vahdet Pervan <vpervan22@gmail.com>
2025-09-28 23:35:45 +0200

    Remove private method.

--- ruby/lib/kata/models/offer.rb
+++ ruby/lib/kata/models/offer.rb
@@ -17,12 +17,4 @@ class Kata::Offer
 
     calculate_five_for_amount_discount(quantity:, unit_price:) if offer_type == Kata::SpecialOfferType::FIVE_FOR_AMOUNT && quantity.to_i >= 5
   end
-
-  private
-
-  def calculate_five_for_amount_discount(quantity:, unit_price:)
-    item_units_for_discount = quantity.to_i / 5
-    discount_total = unit_price * quantity - (argument * item_units_for_discount + quantity.to_i % 5 * unit_price)
-    Kata::Discount.new(product, 5.to_s + " for " + argument.to_s, discount_total)
-  end
 end
