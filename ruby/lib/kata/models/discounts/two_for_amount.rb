efca24e3aec394a1e646b4b3b42f96e3dd11a03f
Vahdet Pervan <vpervan22@gmail.com>
2025-09-29 01:57:57 +0200

    Replace argument attr reader with instance variable.

--- ruby/lib/kata/models/discounts/two_for_amount.rb
+++ ruby/lib/kata/models/discounts/two_for_amount.rb
@@ -1,6 +1,6 @@
 module Kata::Discounts
   class TwoForAmount
-    attr_reader :argument, :quantity, :unit_price, :product
+    attr_reader :quantity, :unit_price, :product
 
     def initialize(argument:, quantity:, unit_price:, product:)
       @argument = argument
@@ -10,9 +10,9 @@ module Kata::Discounts
     end
 
     def calculate
-      total = argument * (quantity.to_i / 2) + quantity.to_i % 2 * unit_price
+      total = @argument * (quantity.to_i / 2) + quantity.to_i % 2 * unit_price
       discount_n = unit_price * quantity - total
-      Kata::Discount.new(product, "2 for " + argument.to_s, discount_n)
+      Kata::Discount.new(product, "2 for " + @argument.to_s, discount_n)
     end
   end
 end
