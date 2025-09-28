7bf7922850c986df7a4d92b2347198dab09fc747
Vahdet Pervan <vpervan22@gmail.com>
2025-09-28 23:23:51 +0200

    New constructor for three for two discount class.

--- ruby/lib/kata/models/discounts/three_for_two.rb
+++ ruby/lib/kata/models/discounts/three_for_two.rb
@@ -1,7 +1,10 @@
 module Kata::Discounts
   class ThreeForTwo
-    def initialize
-
+    attr_reader :quantity, :unit_price, :product
+    def initialize(quantity:, unit_price:, product:)
+      @quantity = quantity
+      @unit_price = unit_price
+      @product = product
     end
   end
 end
