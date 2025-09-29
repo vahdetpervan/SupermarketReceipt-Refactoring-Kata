61b1b283ff64d88920e09dbcb3e6d9b132f32ee2
Vahdet Pervan <vpervan22@gmail.com>
2025-09-29 23:49:25 +0200

    Pass optional params to calculate method for three for two.

--- ruby/lib/kata/models/discounts/three_for_two.rb
+++ ruby/lib/kata/models/discounts/three_for_two.rb
@@ -7,7 +7,7 @@ module Kata::Discounts
       @product = product
     end
 
-    def calculate
+    def calculate(quantity: nil, unit_price: nil)
       item_units_for_discount = @quantity.to_i / 3
       discount_amount = @quantity * @unit_price - ((item_units_for_discount * 2 * @unit_price) + @quantity.to_i % 3 * @unit_price)
       Kata::Discount.new(product, "3 for 2", discount_amount)
