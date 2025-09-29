702141a41e384bb8f61dadca3955a179523060e2
Vahdet Pervan <vpervan22@gmail.com>
2025-09-29 23:59:43 +0200

    Remove old constructor args

--- ruby/lib/kata/models/discounts/ten_percent.rb
+++ ruby/lib/kata/models/discounts/ten_percent.rb
@@ -2,7 +2,7 @@ module Kata::Discounts
   class TenPercent
     attr_reader :argument, :product
 
-    def initialize(quantity: nil, unit_price: nil, argument:, product:)
+    def initialize(argument:, product:)
       @argument = argument
       @product = product
     end
