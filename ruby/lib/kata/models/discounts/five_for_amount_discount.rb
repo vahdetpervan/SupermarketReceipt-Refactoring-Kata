93b1419960164ae601927a97523c2c520852084d
Vahdet Pervan <vpervan22@gmail.com>
2025-09-30 00:06:36 +0200

    Use ivars instead.

--- ruby/lib/kata/models/discounts/five_for_amount_discount.rb
+++ ruby/lib/kata/models/discounts/five_for_amount_discount.rb
@@ -1,7 +1,5 @@
 module Kata::Discounts
   class FiveForAmountDiscount
-    attr_reader :argument, :product
-
     def initialize(argument:, product:)
       @argument = argument
       @product = product
@@ -9,8 +7,8 @@ module Kata::Discounts
 
     def calculate(quantity:, unit_price:)
       item_units_for_discount = quantity.to_i / 5
-      discount_total = unit_price * quantity - (argument * item_units_for_discount + quantity.to_i % 5 * unit_price)
-      Kata::Discount.new(product, 5.to_s + " for " + argument.to_s, discount_total)
+      discount_total = unit_price * quantity - (@argument * item_units_for_discount + quantity.to_i % 5 * unit_price)
+      Kata::Discount.new(@product, 5.to_s + " for " + @argument.to_s, discount_total)
     end
   end
 end
