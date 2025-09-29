a53d021506a39180d22e0d3fa74434604b37379a
Vahdet Pervan <vpervan22@gmail.com>
2025-09-29 23:58:55 +0200

    Set defaults for constructor args.

--- ruby/lib/kata/models/discounts/ten_percent.rb
+++ ruby/lib/kata/models/discounts/ten_percent.rb
@@ -2,7 +2,7 @@ module Kata::Discounts
   class TenPercent
     attr_reader :argument, :product
 
-    def initialize(quantity:, unit_price:, argument:, product:)
+    def initialize(quantity: nil, unit_price: nil, argument:, product:)
       @argument = argument
       @product = product
     end
