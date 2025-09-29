37ef1ef36faa192194df881751d01cc014a228dd
Vahdet Pervan <vpervan22@gmail.com>
2025-09-30 00:04:20 +0200

    Remove ivars.

--- ruby/lib/kata/models/discounts/five_for_amount_discount.rb
+++ ruby/lib/kata/models/discounts/five_for_amount_discount.rb
@@ -3,8 +3,6 @@ module Kata::Discounts
     attr_reader :quantity, :unit_price, :argument, :product
 
     def initialize(quantity:, unit_price:, argument:, product:)
-      @quantity = quantity
-      @unit_price = unit_price
       @argument = argument
       @product = product
     end
