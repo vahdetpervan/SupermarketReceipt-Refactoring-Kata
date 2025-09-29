1cecb7eaa09e91c2af32d769772377dcdbc978df
Vahdet Pervan <vpervan22@gmail.com>
2025-09-29 23:30:24 +0200

    New case for three for two.

--- ruby/lib/kata/models/offer.rb
+++ ruby/lib/kata/models/offer.rb
@@ -12,8 +12,9 @@ class Kata::Offer
     case @offer_type
     when Kata::SpecialOfferType::TWO_FOR_AMOUNT
       return Kata::Discounts::TwoForAmount.new(argument:, quantity:, unit_price:, product:).calculate
+    when Kata::SpecialOfferType::THREE_FOR_TWO
+      return Kata::Discounts::ThreeForTwo.new(quantity:, unit_price:, product:).calculate
     end
-    return Kata::Discounts::ThreeForTwo.new(quantity:, unit_price:, product:).calculate if offer_type == Kata::SpecialOfferType::THREE_FOR_TWO
     return Kata::Discounts::TenPercent.new(quantity:, unit_price:, argument:, product:).calculate if offer_type == Kata::SpecialOfferType::TEN_PERCENT_DISCOUNT
 
     Kata::Discounts::FiveForAmountDiscount.new(quantity:, unit_price:, argument:, product:).calculate if offer_type == Kata::SpecialOfferType::FIVE_FOR_AMOUNT && quantity.to_i >= 5
