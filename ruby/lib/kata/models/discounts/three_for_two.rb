e35bb945070ff2c4b20310cdb0dd0d84a64e036e
Vahdet Pervan <vpervan22@gmail.com>
2025-09-29 23:53:36 +0200

    Remove constructor args in three for two class.

--- ruby/lib/kata/models/discounts/three_for_two.rb
+++ ruby/lib/kata/models/discounts/three_for_two.rb
@@ -1,7 +1,7 @@
 module Kata::Discounts
   class ThreeForTwo
     attr_reader :product
-    def initialize(quantity: nil, unit_price: nil, product:)
+    def initialize(product:)
       @product = product
     end
 
