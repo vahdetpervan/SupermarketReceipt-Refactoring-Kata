b008f7431fd9a646f1045b9cf732e5c6f8534859
Vahdet Pervan <vpervan22@gmail.com>
2025-09-29 23:51:52 +0200

    Remove unused instance variables.

--- ruby/lib/kata/models/discounts/three_for_two.rb
+++ ruby/lib/kata/models/discounts/three_for_two.rb
@@ -2,8 +2,6 @@ module Kata::Discounts
   class ThreeForTwo
     attr_reader :quantity, :unit_price, :product
     def initialize(quantity:, unit_price:, product:)
-      @quantity = quantity
-      @unit_price = unit_price
       @product = product
     end
 
