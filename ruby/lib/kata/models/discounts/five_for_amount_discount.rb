6b5cdd2eb68b92df2c800c7b1989c89216f7238f
Vahdet Pervan <vpervan22@gmail.com>
2025-09-30 00:09:27 +0200

    Delegate conditional to five for amount discount class.

--- ruby/lib/kata/models/discounts/five_for_amount_discount.rb
+++ ruby/lib/kata/models/discounts/five_for_amount_discount.rb
@@ -6,6 +6,8 @@ module Kata::Discounts
     end
 
     def calculate(quantity:, unit_price:)
+      return unless quantity.to_i >= 5
+
       item_units_for_discount = quantity.to_i / 5
       discount_total = unit_price * quantity - (@argument * item_units_for_discount + quantity.to_i % 5 * unit_price)
       Kata::Discount.new(@product, 5.to_s + " for " + @argument.to_s, discount_total)
