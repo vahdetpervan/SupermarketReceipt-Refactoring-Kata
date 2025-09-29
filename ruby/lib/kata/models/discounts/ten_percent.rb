e2a3407fe5e252721d0d7fbd686bb08d99d19385
Vahdet Pervan <vpervan22@gmail.com>
2025-09-29 23:58:21 +0200

    Remove defaults.

--- ruby/lib/kata/models/discounts/ten_percent.rb
+++ ruby/lib/kata/models/discounts/ten_percent.rb
@@ -9,7 +9,7 @@ module Kata::Discounts
       @product = product
     end
 
-    def calculate(quantity: nil, unit_price: nil)
+    def calculate(quantity:, unit_price:)
       Kata::Discount.new(product, argument.to_s + "% off", quantity * unit_price * argument / 100.0)
     end
   end
