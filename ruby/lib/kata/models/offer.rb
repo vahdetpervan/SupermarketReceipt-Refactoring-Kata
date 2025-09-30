d41fe05a685ee8a4dc03cfac6a81e24cade8ab55
Vahdet Pervan <vpervan22@gmail.com>
2025-09-30 17:56:05 +0200

    Remove return.

--- ruby/lib/kata/models/offer.rb
+++ ruby/lib/kata/models/offer.rb
@@ -9,7 +9,7 @@ class Kata::Offer
   end
 
   def discount(quantity:, unit_price:)
-    return discount_classes[@offer_type].new({ argument:, product: }).calculate(quantity:, unit_price:) if discount_classes[@offer_type]
+    discount_classes[@offer_type].new({ argument:, product: }).calculate(quantity:, unit_price:) if discount_classes[@offer_type]
   end
 
   private
