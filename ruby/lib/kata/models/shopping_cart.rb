898c62242639b22b794535e77d4b81e33dc5e67b
Vahdet Pervan <vpervan22@gmail.com>
2025-09-30 19:52:09 +0200

    Use plus equals operator instead.

--- ruby/lib/kata/models/shopping_cart.rb
+++ ruby/lib/kata/models/shopping_cart.rb
@@ -7,7 +7,7 @@ class Kata::ShoppingCart
 
   def add_item_quantity(product, quantity = 1)
     @items << Kata::ProductQuantity.new(product, quantity)
-    return @product_quantities[product] = @product_quantities[product] + quantity if @product_quantities.key?(product)
+    return @product_quantities[product] += quantity if @product_quantities.key?(product)
 
     @product_quantities[product] = quantity
   end
