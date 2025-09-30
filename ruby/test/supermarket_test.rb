f033777154b10302517cc6ad5060c73374f93453
Vahdet Pervan <vpervan22@gmail.com>
2025-09-30 19:46:13 +0200

    Use add_item_quantity method instead.

--- ruby/test/supermarket_test.rb
+++ ruby/test/supermarket_test.rb
@@ -48,11 +48,11 @@ class SupermarketTest < Minitest::Test
   end
 
   def test_buy_five_get_one_free
-    @the_cart.add_item(@toothbrush)
-    @the_cart.add_item(@toothbrush)
-    @the_cart.add_item(@toothbrush)
-    @the_cart.add_item(@toothbrush)
-    @the_cart.add_item(@toothbrush)
+    @the_cart.add_item_quantity(@toothbrush)
+    @the_cart.add_item_quantity(@toothbrush)
+    @the_cart.add_item_quantity(@toothbrush)
+    @the_cart.add_item_quantity(@toothbrush)
+    @the_cart.add_item_quantity(@toothbrush)
     @teller.add_special_offer(Kata::SpecialOfferType::THREE_FOR_TWO, @toothbrush, @catalog.unit_price(@toothbrush))
     receipt = @teller.checks_out_articles_from(@the_cart)
     verify Kata::ReceiptPrinter.new(40).print_receipt(receipt)
