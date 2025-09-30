0fc4292855a793fc41f54c48abb7d385c4750464
Vahdet Pervan <vpervan22@gmail.com>
2025-09-30 23:49:33 +0200

    Remove private method.

--- ruby/lib/kata/models/offer.rb
+++ ruby/lib/kata/models/offer.rb
@@ -17,15 +17,4 @@ class Kata::Offer
   def discount(quantity:, unit_price:)
     @discount_classes[@offer_type].new({ argument:, product: }).calculate(quantity:, unit_price:)
   end
-
-  private
-
-  def discount_classes
-    {
-      Kata::SpecialOfferType::TWO_FOR_AMOUNT => Kata::Discounts::TwoForAmount,
-      Kata::SpecialOfferType::TEN_PERCENT_DISCOUNT => Kata::Discounts::TenPercent,
-      Kata::SpecialOfferType::FIVE_FOR_AMOUNT => Kata::Discounts::FiveForAmount,
-      Kata::SpecialOfferType::THREE_FOR_TWO => Kata::Discounts::ThreeForTwo,
-    }
-  end
 end
