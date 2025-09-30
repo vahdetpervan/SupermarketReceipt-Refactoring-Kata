a884f21b4d89c8d9ea1dd0d59241b7d4cdf4b184
Vahdet Pervan <vpervan22@gmail.com>
2025-09-30 19:43:11 +0200

    Use add_item_quantity instead.

--- ruby/test/supermarket_test.rb
+++ ruby/test/supermarket_test.rb
@@ -26,7 +26,7 @@ class SupermarketTest < Minitest::Test
   end
 
   def test_one_normal_item
-    @the_cart.add_item(@toothbrush)
+    @the_cart.add_item_quantity(@toothbrush, 1)
     receipt = @teller.checks_out_articles_from(@the_cart)
     verify Kata::ReceiptPrinter.new(40).print_receipt(receipt)
   end
