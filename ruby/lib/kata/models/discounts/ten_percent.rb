1b65cfac7d23e6219f27685789194f29e7e63580
Vahdet Pervan <vpervan22@gmail.com>
2025-09-30 17:51:51 +0200

    Use values from hash.

--- ruby/lib/kata/models/discounts/ten_percent.rb
+++ ruby/lib/kata/models/discounts/ten_percent.rb
@@ -1,8 +1,8 @@
 module Kata::Discounts
   class TenPercent
     def initialize(options = {}, argument: nil, product: nil)
-      @argument = argument
-      @product = product
+      @argument = options[:argument]
+      @product = options[:product]
     end
 
     def calculate(quantity:, unit_price:)
