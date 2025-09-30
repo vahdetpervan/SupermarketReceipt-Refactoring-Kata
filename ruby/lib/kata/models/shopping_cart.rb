dca034a56c473ed5fa507d161f791ec8d72834e4
Vahdet Pervan <vpervan22@gmail.com>
2025-09-30 20:00:10 +0200

    Last line no longer needed, and no return statement needed anymore.

--- ruby/lib/kata/models/shopping_cart.rb
+++ ruby/lib/kata/models/shopping_cart.rb
@@ -7,9 +7,7 @@ class Kata::ShoppingCart
 
   def add_item_quantity(product, quantity = 1)
     @items << Kata::ProductQuantity.new(product, quantity)
-    return @product_quantities[product] += quantity
-
-    @product_quantities[product] = quantity
+    @product_quantities[product] += quantity
   end
 
   def handle_offers(receipt, offers, catalog)
