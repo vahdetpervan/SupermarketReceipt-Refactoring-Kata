72cb9da68309ff878e9af29bba15e1175d000ec0
Vahdet Pervan <vpervan22@gmail.com>
2025-09-30 17:36:49 +0200

    Remove case and when.

--- ruby/lib/kata/models/offer.rb
+++ ruby/lib/kata/models/offer.rb
@@ -10,10 +10,8 @@ class Kata::Offer
 
   def discount(quantity:, unit_price:)
     return discount_classes[@offer_type].new(argument:, product:).calculate(quantity:, unit_price:) if discount_classes[@offer_type]
-    case @offer_type
-    when Kata::SpecialOfferType::THREE_FOR_TWO
-      Kata::Discounts::ThreeForTwo.new(product:).calculate(quantity:, unit_price:) if @offer_type == Kata::SpecialOfferType::THREE_FOR_TWO
-    end
+
+    Kata::Discounts::ThreeForTwo.new(product:).calculate(quantity:, unit_price:) if @offer_type == Kata::SpecialOfferType::THREE_FOR_TWO
   end
 
   private
