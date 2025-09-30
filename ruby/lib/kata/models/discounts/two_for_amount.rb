55ec31cd4fcd9c6ea441599e27b90423eaaa80df
Vahdet Pervan <vpervan22@gmail.com>
2025-09-30 17:49:53 +0200

    Set nil values.

--- ruby/lib/kata/models/discounts/two_for_amount.rb
+++ ruby/lib/kata/models/discounts/two_for_amount.rb
@@ -1,6 +1,6 @@
 module Kata::Discounts
   class TwoForAmount
-    def initialize(options = {}, argument:, product:)
+    def initialize(options = {}, argument: nil, product: nil)
       @argument = options[:argument]
       @product = options[:product]
     end
