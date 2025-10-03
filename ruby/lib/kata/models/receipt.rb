9fb060dbaa53222cd054f6bd0b29a3c6afa408e5
Vahdet Pervan <vpervan22@gmail.com>
2025-10-03 12:52:10 +0200

    Remove commented out code.

--- ruby/lib/kata/models/receipt.rb
+++ ruby/lib/kata/models/receipt.rb
@@ -9,7 +9,6 @@ class Kata::Receipt
   def total_price
     total = 0
     total += @items.sum(&:total_price)
-    # @items.each { |item| total += item.total_price }
     @discounts.each { |discount| total -= discount.discount_amount }
     total
   end
