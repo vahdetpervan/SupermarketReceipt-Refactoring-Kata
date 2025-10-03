fe2506e8b9499f3fb99965aac9c005f508881d69
Vahdet Pervan <vpervan22@gmail.com>
2025-10-03 13:11:23 +0200

    Move building of receipt to constructor.

--- ruby/lib/kata/models/teller.rb
+++ ruby/lib/kata/models/teller.rb
@@ -3,6 +3,7 @@ class Kata::Teller
   def initialize(catalog)
     @catalog = catalog
     @offers = {}
+    @receipt = Kata::Receipt.new
   end
 
   def add_special_offer(offer_type, product, argument)
@@ -10,18 +11,17 @@ class Kata::Teller
   end
 
   def checks_out_articles_from(the_cart)
-    receipt = Kata::Receipt.new
     product_quantities = the_cart.items
     for pq in product_quantities do
       p = pq.product
       quantity = pq.quantity
       unit_price = @catalog.unit_price(p)
       price = quantity * unit_price
-      receipt.add_product(p, quantity, unit_price, price)
+      @receipt.add_product(p, quantity, unit_price, price)
     end
-    the_cart.handle_offers(receipt, @offers, @catalog)
+    the_cart.handle_offers(@receipt, @offers, @catalog)
 
-    receipt
+    @receipt
   end
 
 end
