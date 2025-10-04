50518b0ad0063a9cf6192265b7b1895a9c35ddaf
Vahdet Pervan <vpervan22@gmail.com>
2025-10-04 21:31:30 +0200

    Use instance method instead.

--- ruby/lib/kata/receipt_printer.rb
+++ ruby/lib/kata/receipt_printer.rb
@@ -37,7 +37,7 @@ class Kata::ReceiptPrinter
     result.concat("\n")
     price_presentation = "%.2f" % receipt.total_price.to_f
     total = "Total: "
-    whitespace = self.class.whitespace(@columns - total.size - price_presentation.size)
+    whitespace = whitespace(@columns - total.size - price_presentation.size)
     result.concat(total, whitespace, price_presentation)
     result.to_s
   end
