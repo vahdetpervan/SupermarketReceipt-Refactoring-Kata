20d6ebc06d37ce4df2d18392fa99849d1b2d4140
Vahdet Pervan <vpervan22@gmail.com>
2025-10-04 21:41:16 +0200

    Duplicate static method to instance.

--- ruby/lib/kata/receipt_printer.rb
+++ ruby/lib/kata/receipt_printer.rb
@@ -42,6 +42,10 @@ class Kata::ReceiptPrinter
     result.to_s
   end
 
+  def present_quantity(item)
+    Kata::ProductUnit::EACH == item.product.unit ? '%x' % item.quantity.to_i : '%.3f' % item.quantity
+  end
+
   def self.present_quantity(item)
     Kata::ProductUnit::EACH == item.product.unit ? '%x' % item.quantity.to_i : '%.3f' % item.quantity
   end
