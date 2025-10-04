8157ce99f2eda931046c57d1be688c4d4832712d
Vahdet Pervan <vpervan22@gmail.com>
2025-10-04 21:40:28 +0200

    No need for return statement.

--- ruby/lib/kata/receipt_printer.rb
+++ ruby/lib/kata/receipt_printer.rb
@@ -47,9 +47,6 @@ class Kata::ReceiptPrinter
   end
 
   def whitespace(whitespace_size)
-    return ' ' * whitespace_size
-    whitespace = ''
-    whitespace_size.times { whitespace.concat(' ') }
-    whitespace
+    ' ' * whitespace_size
   end
 end
