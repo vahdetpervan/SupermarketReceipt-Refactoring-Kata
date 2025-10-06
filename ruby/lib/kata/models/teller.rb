7e531cdbaedce587f235cfbf9e2eb8240c750202
Vahdet Pervan <vpervan22@gmail.com>
2025-10-06 23:50:23 +0200

    Use offer_for method instead.

--- ruby/lib/kata/models/teller.rb
+++ ruby/lib/kata/models/teller.rb
@@ -6,7 +6,16 @@ class Kata::Teller
   end
 
   def add_special_offer(offer_type, product, argument)
-    @offers[product] = Kata::Offer.new(offer_type, product, argument)
+    @offers[product] = offer_for(offer_type, product, argument)
+  end
+
+  def offer_for(offer_type, product, argument)
+    {
+      Kata::SpecialOfferType::TWO_FOR_AMOUNT => Kata::Discounts::TwoForAmount,
+      Kata::SpecialOfferType::TEN_PERCENT_DISCOUNT => Kata::Discounts::TenPercent,
+      Kata::SpecialOfferType::FIVE_FOR_AMOUNT => Kata::Discounts::FiveForAmount,
+      Kata::SpecialOfferType::THREE_FOR_TWO => Kata::Discounts::ThreeForTwo
+    }[offer_type].new({ argument:, product: })
   end
 
   def checks_out_articles_from(the_cart)
