c97f005acc02f1dfbc8e17fa3c1a825156af3243
Vahdet Pervan <vpervan22@gmail.com>
2025-10-07 03:01:45 +0200

    Inline x

--- ruby/lib/kata/models/shopping_cart.rb
+++ ruby/lib/kata/models/shopping_cart.rb
@@ -47,9 +47,8 @@ class Kata::ShoppingCart
           discount = Kata::Discount.new(product, offers[product].argument.to_s + "% off", quantity * unit_price * offers[product].argument / 100.0)
         end
         if offers[product].offer_type == Kata::SpecialOfferType::FIVE_FOR_AMOUNT && quantity.to_i >= 5
-          x = 5
-          discount_total = unit_price * quantity - (offers[product].argument * (quantity.to_i / x) + quantity.to_i % 5 * unit_price)
-          discount = Kata::Discount.new(product, x.to_s + " for " + offers[product].argument.to_s, discount_total)
+          discount_total = unit_price * quantity - (offers[product].argument * (quantity.to_i / 5) + quantity.to_i % 5 * unit_price)
+          discount = Kata::Discount.new(product, 5.to_s + " for " + offers[product].argument.to_s, discount_total)
         end
 
         receipt.add_discount(discount) if discount
