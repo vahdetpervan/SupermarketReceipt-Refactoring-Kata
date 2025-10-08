7a6c5a4adf772f89a68a0826aebcadf180e1870d
Vahdet Pervan <vpervan22@gmail.com>
2025-10-08 16:46:11 +0200

    Use ivars instead

--- ruby/lib/kata/models/discounts/two_for_amount.rb
+++ ruby/lib/kata/models/discounts/two_for_amount.rb
@@ -7,9 +7,9 @@ module Kata::Discounts
       @quantity = options[:quantity]
     end
     def calculate(offers, product, unit_price, quantity)
-      total = offers[product].argument * (quantity.to_i / 2) + quantity.to_i % 2 * unit_price
-      discount_n = unit_price * quantity - total
-      Kata::Discount.new(product, "2 for " + offers[product].argument.to_s, discount_n)
+      total = @offers[@product].argument * (@quantity.to_i / 2) + @quantity.to_i % 2 * @unit_price
+      discount_n = @unit_price * @quantity - total
+      Kata::Discount.new(@product, "2 for " + @offers[@product].argument.to_s, discount_n)
     end
   end
 end
