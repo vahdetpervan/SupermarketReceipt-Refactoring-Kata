173682a53e1c756b9c34fb2fd14e834a841a6d96
Vahdet Pervan <vpervan22@gmail.com>
2025-10-08 16:52:30 +0200

    Need argument as well

--- ruby/lib/kata/models/shopping_cart.rb
+++ ruby/lib/kata/models/shopping_cart.rb
@@ -27,7 +27,7 @@ class Kata::ShoppingCart
   private
 
   def two_for_amount_discount(offers, product, unit_price, quantity)
-    Kata::Discounts::TwoForAmount.new({offers:, product:, unit_price:, quantity:, argument: offers[product]}).calculate
+    Kata::Discounts::TwoForAmount.new({offers:, product:, unit_price:, quantity:, argument: offers[product].argument}).calculate
   end
 
   def three_for_two_discount(product, unit_price, quantity)
