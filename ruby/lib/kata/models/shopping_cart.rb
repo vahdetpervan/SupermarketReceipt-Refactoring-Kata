a01869adb46165323c101774297dda2efe622168
Vahdet Pervan <vpervan22@gmail.com>
2025-10-08 16:41:18 +0200

    Use new discount class containing five for amount logic.

--- ruby/lib/kata/models/shopping_cart.rb
+++ ruby/lib/kata/models/shopping_cart.rb
@@ -39,10 +39,7 @@ class Kata::ShoppingCart
   end
 
   def five_for_amount_discount(offers, product, unit_price, quantity)
-    return unless quantity.to_i >= 5
-
-    discount_total = unit_price * quantity - (offers[product].argument * (quantity.to_i / 5) + quantity.to_i % 5 * unit_price)
-    Kata::Discount.new(product, 5.to_s + " for " + offers[product].argument.to_s, discount_total)
+    Kata::Discounts::FiveForAmount.new.calculate(offers, product, unit_price, quantity)
   end
 
 end
