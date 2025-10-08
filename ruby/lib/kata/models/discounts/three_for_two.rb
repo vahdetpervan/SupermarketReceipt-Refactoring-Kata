a04f18eebb41b1c458a07719a068d7a8cb6ce303
Vahdet Pervan <vpervan22@gmail.com>
2025-10-08 16:55:58 +0200

    No need for the parenthesis

--- ruby/lib/kata/models/discounts/three_for_two.rb
+++ ruby/lib/kata/models/discounts/three_for_two.rb
@@ -6,9 +6,9 @@ module Kata::Discounts
       @quantity = options[:quantity]
     end
 
-    def calculate(product, unit_price, quantity)
-      discount_amount = quantity * unit_price - ((quantity.to_i / 3 * 2 * unit_price) + quantity.to_i % 3 * unit_price)
-      Kata::Discount.new(product, "3 for 2", discount_amount)
+    def calculate
+      discount_amount = @quantity * @unit_price - ((@quantity.to_i / 3 * 2 * @unit_price) + @quantity.to_i % 3 * @unit_price)
+      Kata::Discount.new(@product, "3 for 2", discount_amount)
     end
   end
 end
