7d2bca9bc7e2a7d0e8a654531f7d227da56bff0b
Vahdet Pervan <vpervan22@gmail.com>
2025-10-08 17:06:24 +0200

    Use argument

--- ruby/lib/kata/models/discounts/five_for_amount.rb
+++ ruby/lib/kata/models/discounts/five_for_amount.rb
@@ -3,8 +3,8 @@ module Kata::Discounts
     def calculate(offers, product, unit_price, quantity, argument = nil)
       return unless quantity.to_i >= 5
 
-      discount_total = unit_price * quantity - (offers[product].argument * (quantity.to_i / 5) + quantity.to_i % 5 * unit_price)
-      Kata::Discount.new(product, 5.to_s + " for " + offers[product].argument.to_s, discount_total)
+      discount_total = unit_price * quantity - (argument * (quantity.to_i / 5) + quantity.to_i % 5 * unit_price)
+      Kata::Discount.new(product, 5.to_s + " for " + argument.to_s, discount_total)
     end
   end
 end
