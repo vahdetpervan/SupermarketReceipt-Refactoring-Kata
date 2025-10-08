3064c13a27443b928f0f23328338ae39ae3ba81c
Vahdet Pervan <vpervan22@gmail.com>
2025-10-08 16:44:31 +0200

    Pass in args via constructor

--- ruby/lib/kata/models/discounts/two_for_amount.rb
+++ ruby/lib/kata/models/discounts/two_for_amount.rb
@@ -1,5 +1,11 @@
 module Kata::Discounts
   class TwoForAmount
+    def initialize(options = {})
+      @offers = options[:offers]
+      @product = options[:product]
+      @unit_price = options[:unit_price]
+      @quantity = options[:quantity]
+    end
     def calculate(offers, product, unit_price, quantity)
       total = offers[product].argument * (quantity.to_i / 2) + quantity.to_i % 2 * unit_price
       discount_n = unit_price * quantity - total
