507de7f98e5d2c22d79bce55251bbdaf8aff3c5c
Vahdet Pervan <vpervan22@gmail.com>
2025-10-08 15:16:27 +0200

    Remove quantity check for three for two

--- ruby/lib/kata/models/shopping_cart.rb
+++ ruby/lib/kata/models/shopping_cart.rb
@@ -16,7 +16,7 @@ class Kata::ShoppingCart
       next unless offers.key?(product)
 
       discount = two_for_amount_discount(offers, product, catalog.unit_price(product), quantity) if offers[product].offer_type == Kata::SpecialOfferType::TWO_FOR_AMOUNT
-      discount = three_for_two_discount(product, catalog.unit_price(product), quantity) if offers[product].offer_type == Kata::SpecialOfferType::THREE_FOR_TWO && quantity.to_i > 2
+      discount = three_for_two_discount(product, catalog.unit_price(product), quantity) if offers[product].offer_type == Kata::SpecialOfferType::THREE_FOR_TWO
       discount = ten_percent_discount(offers, product, catalog.unit_price(product), quantity) if offers[product].offer_type == Kata::SpecialOfferType::TEN_PERCENT_DISCOUNT
       discount = five_for_amount_discount(offers, product, catalog.unit_price(product), quantity) if offers[product].offer_type == Kata::SpecialOfferType::FIVE_FOR_AMOUNT
 
