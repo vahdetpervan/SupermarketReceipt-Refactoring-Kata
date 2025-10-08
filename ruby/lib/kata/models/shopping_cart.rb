4407a8f5ecdb326e08cec222a571cc3dc7049afb
Vahdet Pervan <vpervan22@gmail.com>
2025-10-08 15:10:37 +0200

    Use guard clause

--- ruby/lib/kata/models/shopping_cart.rb
+++ ruby/lib/kata/models/shopping_cart.rb
@@ -18,9 +18,7 @@ class Kata::ShoppingCart
       unit_price = catalog.unit_price(product)
       discount = two_for_amount_discount(offers, product, unit_price, quantity) if offers[product].offer_type == Kata::SpecialOfferType::TWO_FOR_AMOUNT
       discount = three_for_two_discount(product, unit_price, quantity) if offers[product].offer_type == Kata::SpecialOfferType::THREE_FOR_TWO && quantity.to_i > 2
-      if offers[product].offer_type == Kata::SpecialOfferType::TEN_PERCENT_DISCOUNT
-        discount = ten_percent_discount(offers, product, unit_price, quantity)
-      end
+      discount = ten_percent_discount(offers, product, unit_price, quantity) if offers[product].offer_type == Kata::SpecialOfferType::TEN_PERCENT_DISCOUNT
       if offers[product].offer_type == Kata::SpecialOfferType::FIVE_FOR_AMOUNT && quantity.to_i >= 5
         discount = five_for_amount_discount(offers, product, unit_price, quantity)
       end
