9d20f499284d114af01963a16195e3a03d7047b1
Vahdet Pervan <vpervan22@gmail.com>
2025-10-08 16:53:01 +0200

    Use @argument instance var

--- ruby/lib/kata/models/discounts/two_for_amount.rb
+++ ruby/lib/kata/models/discounts/two_for_amount.rb
@@ -1,15 +1,16 @@
 module Kata::Discounts
   class TwoForAmount
     def initialize(options = {})
-      @offers = options[:offers]
+      @argument = options[:argument]
       @product = options[:product]
       @unit_price = options[:unit_price]
       @quantity = options[:quantity]
     end
+
     def calculate
-      total = @offers[@product].argument * (@quantity.to_i / 2) + @quantity.to_i % 2 * @unit_price
+      total = @argument * (@quantity.to_i / 2) + @quantity.to_i % 2 * @unit_price
       discount_n = @unit_price * @quantity - total
-      Kata::Discount.new(@product, "2 for " + @offers[@product].argument.to_s, discount_n)
+      Kata::Discount.new(@product, "2 for " + @argument.to_s, discount_n)
     end
   end
 end
