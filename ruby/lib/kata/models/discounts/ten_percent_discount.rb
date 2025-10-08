c7ffb319952d04d352089d6a5a0eba8f5e6ea353
Vahdet Pervan <vpervan22@gmail.com>
2025-10-08 17:02:00 +0200

    Introduce constructor for TenPercentDiscount class

--- ruby/lib/kata/models/discounts/ten_percent_discount.rb
+++ ruby/lib/kata/models/discounts/ten_percent_discount.rb
@@ -1,5 +1,12 @@
 module Kata::Discounts
   class TenPercentDiscount
+    def initialize(options = {})
+      @product = options[:product]
+      @argument = options[:argument]
+      @unit_price = options[:unit_price]
+      @quantity = options[:quantity]
+    end
+
     def calculate(product, argument, unit_price, quantity)
       Kata::Discount.new(product, argument.to_s + "% off", quantity * unit_price * argument / 100.0)
     end
