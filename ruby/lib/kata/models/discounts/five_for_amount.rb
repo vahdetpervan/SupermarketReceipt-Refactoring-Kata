408a2f40726b9d6d62f7905aabf450a3f60cb1b5
Vahdet Pervan <vpervan22@gmail.com>
2025-10-08 17:10:02 +0200

    Use ivars

--- ruby/lib/kata/models/discounts/five_for_amount.rb
+++ ruby/lib/kata/models/discounts/five_for_amount.rb
@@ -8,10 +8,10 @@ module Kata::Discounts
     end
 
     def calculate(product, argument, unit_price, quantity)
-      return unless quantity.to_i >= 5
+      return unless @quantity.to_i >= 5
 
-      discount_total = unit_price * quantity - (argument * (quantity.to_i / 5) + quantity.to_i % 5 * unit_price)
-      Kata::Discount.new(product, 5.to_s + " for " + argument.to_s, discount_total)
+      discount_total = @unit_price * @quantity - (@argument * (@quantity.to_i / 5) + @quantity.to_i % 5 * @unit_price)
+      Kata::Discount.new(@product, 5.to_s + " for " + @argument.to_s, discount_total)
     end
   end
 end
