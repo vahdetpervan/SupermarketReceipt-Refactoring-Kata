110b1cf58f71be15f8d3195c22b631fdd0fb2710
Vahdet Pervan <vpervan22@gmail.com>
2025-10-08 16:40:28 +0200

    Copy five for amount logic into new discount class

---
+++ ruby/lib/kata/models/discounts/five_for_amount.rb
@@ -0,0 +1,10 @@
+module Kata::Discounts
+  class FiveForAmount
+    def calculate(offers, product, unit_price, quantity)
+      return unless quantity.to_i >= 5
+
+      discount_total = unit_price * quantity - (offers[product].argument * (quantity.to_i / 5) + quantity.to_i % 5 * unit_price)
+      Kata::Discount.new(product, 5.to_s + " for " + offers[product].argument.to_s, discount_total)
+    end
+  end
+end
