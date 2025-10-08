796ae833c3aed903a9afedbe5780f0cfb2cd5d12
Vahdet Pervan <vpervan22@gmail.com>
2025-10-08 16:57:55 +0200

    Introduce argument param as optional

--- ruby/lib/kata/models/discounts/ten_percent_discount.rb
+++ ruby/lib/kata/models/discounts/ten_percent_discount.rb
@@ -1,6 +1,6 @@
 module Kata::Discounts
   class TenPercentDiscount
-    def calculate(offers, product, unit_price, quantity)
+    def calculate(offers, product, unit_price, quantity, argument = nil)
       Kata::Discount.new(product, offers[product].argument.to_s + "% off", quantity * unit_price * offers[product].argument / 100.0)
     end
   end
--- ruby/lib/kata/models/shopping_cart.rb
+++ ruby/lib/kata/models/shopping_cart.rb
@@ -35,7 +35,7 @@ class Kata::ShoppingCart
   end
 
   def ten_percent_discount(offers, product, unit_price, quantity)
-    Kata::Discounts::TenPercentDiscount.new.calculate(offers, product, unit_price, quantity)
+    Kata::Discounts::TenPercentDiscount.new.calculate(offers, product, unit_price, quantity, offers[product].argument)
   end
 
   def five_for_amount_discount(offers, product, unit_price, quantity)
