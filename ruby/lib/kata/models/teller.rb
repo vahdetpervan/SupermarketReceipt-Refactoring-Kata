5f1f89d20c103a3cb802afe3adcf2b018a21ffc5
Vahdet Pervan <vpervan22@gmail.com>
2025-10-16 20:19:40 +0200

    Use ivar

--- ruby/lib/kata/models/teller.rb
+++ ruby/lib/kata/models/teller.rb
@@ -9,16 +9,15 @@ class Kata::Teller
   def add_special_offer(offer_type, product, argument) = @offers[product] = Kata::Offer.new(offer_type, product, argument)
 
   def checks_out_articles_from(the_cart)
-    receipt = Kata::Receipt.new
     the_cart.product_quantities.each do |product, quantity|
       unit_price = @catalog.unit_price(product)
       price = quantity * unit_price
-      receipt.add_product(product, quantity, unit_price, price) if product.unit == Kata::ProductUnit::KILO
-      quantity.times { receipt.add_product(product, 1, unit_price, unit_price) } if product.unit == Kata::ProductUnit::EACH
+      @receipt.add_product(product, quantity, unit_price, price) if product.unit == Kata::ProductUnit::KILO
+      quantity.times { @receipt.add_product(product, 1, unit_price, unit_price) } if product.unit == Kata::ProductUnit::EACH
     end
-    the_cart.handle_offers(receipt, @offers, @catalog)
+    the_cart.handle_offers(@receipt, @offers, @catalog)
 
-    receipt
+    @receipt
   end
 
 end
