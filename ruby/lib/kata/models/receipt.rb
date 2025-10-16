a5214a9a3b28f62459f994037ca8e574f6404ffb
Vahdet Pervan <vpervan22@gmail.com>
2025-10-16 12:13:21 +0200

    Use single line block instead

--- ruby/lib/kata/models/receipt.rb
+++ ruby/lib/kata/models/receipt.rb
@@ -7,9 +7,7 @@ class Kata::Receipt
 
   def total_price
     total = 0.0
-    @items.each do |item|
-      total += item.total_price
-    end
+    @items.each { |item| total += item.total_price }
     @discounts.each do |discount|
       total -= discount.discount_amount
     end
