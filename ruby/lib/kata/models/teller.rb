f45069f985c605ad951b13d2a48e3d14a763b45d
Vahdet Pervan <vpervan22@gmail.com>
2025-10-16 12:03:31 +0200

    Use .each

--- ruby/lib/kata/models/teller.rb
+++ ruby/lib/kata/models/teller.rb
@@ -12,7 +12,7 @@ class Kata::Teller
   def checks_out_articles_from(the_cart)
     receipt = Kata::Receipt.new
     product_quantities = the_cart.items
-    for pq in product_quantities do
+    product_quantities.each do |pq|
       p = pq.product
       quantity = pq.quantity
       unit_price = @catalog.unit_price(p)
