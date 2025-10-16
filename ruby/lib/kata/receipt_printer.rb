4112e997d199009b1f2a58b72eca925b6ce3ef55
Vahdet Pervan <vpervan22@gmail.com>
2025-10-16 14:09:32 +0200

    No need for .to_f

--- ruby/lib/kata/receipt_printer.rb
+++ ruby/lib/kata/receipt_printer.rb
@@ -35,7 +35,7 @@ class Kata::ReceiptPrinter
       result.concat("\n");
     end
     result.concat("\n")
-    price_presentation = "%.2f" % receipt.total_price.to_f
+    price_presentation = "%.2f" % receipt.total_price
     total = "Total: "
     whitespace = whitespace(@columns - total.size - price_presentation.size)
     result.concat(total, whitespace, price_presentation)
