8157929ada361c997d006088c75e979a9ee17f32
Vahdet Pervan <vpervan22@gmail.com>
2025-10-16 12:19:12 +0200

    Remove unused code and return statement

--- ruby/lib/kata/models/receipt.rb
+++ ruby/lib/kata/models/receipt.rb
@@ -6,11 +6,7 @@ class Kata::Receipt
   end
 
   def total_price
-    return @items.sum(&:total_price) - @discounts.sum(&:discount_amount)
-    total = 0
-    total += @items.sum(&:total_price)
-    total -= @discounts.sum(&:discount_amount)
-    total
+    @items.sum(&:total_price) - @discounts.sum(&:discount_amount)
   end
 
   def add_product(product, quantity, price, total_price) = @items << Kata::ReceiptItem.new(product, quantity, price, total_price)
