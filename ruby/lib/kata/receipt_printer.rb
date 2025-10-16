bbfde8f171c8d1d319b88e95309d26d44a7d4c64
Vahdet Pervan <vpervan22@gmail.com>
2025-10-16 14:11:43 +0200

    Remove if and end

--- ruby/lib/kata/receipt_printer.rb
+++ ruby/lib/kata/receipt_printer.rb
@@ -14,10 +14,7 @@ class Kata::ReceiptPrinter
 
       whitespace_size = @columns - name.size - price.size
       line = name + whitespace(whitespace_size) + price + "\n"
-
-      if item.quantity != 1
-        line += "  " + unit_price + " * " + quantity + "\n" if item.quantity != 1
-      end
+      line += "  " + unit_price + " * " + quantity + "\n" if item.quantity != 1
 
       result.concat(line)
     end
