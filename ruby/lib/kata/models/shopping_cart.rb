7ff6277fd063b947282bff7735160115f71d3366
Vahdet Pervan <vpervan22@gmail.com>
2025-10-16 11:46:00 +0200

    Use discount class from hash instead

--- ruby/lib/kata/models/shopping_cart.rb
+++ ruby/lib/kata/models/shopping_cart.rb
@@ -20,7 +20,8 @@ class Kata::ShoppingCart
       next unless offers.key?(product)
 
       discount = @discount_classes[offers[product].offer_type].new({offers:, product:, unit_price: catalog.unit_price(product), quantity:, argument: offers[product].argument}).calculate if offers[product].offer_type == Kata::SpecialOfferType::TWO_FOR_AMOUNT
-      discount = three_for_two_discount(product, catalog.unit_price(product), quantity) if offers[product].offer_type == Kata::SpecialOfferType::THREE_FOR_TWO
+      discount = @discount_classes[offers[product].offer_type].new({ product:, unit_price: catalog.unit_price(product), quantity: }).calculate if offers[product].offer_type == Kata::SpecialOfferType::THREE_FOR_TWO
+      # discount = three_for_two_discount(product, catalog.unit_price(product), quantity) if offers[product].offer_type == Kata::SpecialOfferType::THREE_FOR_TWO
       discount = ten_percent_discount(offers, product, catalog.unit_price(product), quantity) if offers[product].offer_type == Kata::SpecialOfferType::TEN_PERCENT_DISCOUNT
       discount = five_for_amount_discount(offers, product, catalog.unit_price(product), quantity) if offers[product].offer_type == Kata::SpecialOfferType::FIVE_FOR_AMOUNT
 
