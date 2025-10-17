215cb06c16279a38bdb183bf13b7d1ca8f04e0f9
Vahdet Pervan <vpervan22@gmail.com>
2025-10-17 16:02:32 +0200

    Remove ivars and optional params

--- ruby/lib/kata/models/discounts/ten_percent_discount.rb
+++ ruby/lib/kata/models/discounts/ten_percent_discount.rb
@@ -3,11 +3,9 @@ module Kata::Discounts
     def initialize(options = {})
       @product = options[:product]
       @argument = options[:argument]
-      @unit_price = options[:unit_price]
-      @quantity = options[:quantity]
     end
 
-    def calculate(quantity: nil, unit_price: nil)
+    def calculate(quantity:, unit_price:)
       Kata::Discount.new(@product, @argument.to_s + "% off", quantity * unit_price * @argument / 100.0)
     end
   end
