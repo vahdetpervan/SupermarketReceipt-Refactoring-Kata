296b00f87a889aed7dd1e91929fc0af38d598ec3
Vahdet Pervan <vpervan22@gmail.com>
2025-10-17 16:19:48 +0200

    Use endless method definition syntax

--- ruby/lib/kata/models/shopping_cart.rb
+++ ruby/lib/kata/models/shopping_cart.rb
@@ -1,9 +1,7 @@
 class Kata::ShoppingCart
   attr_reader :product_quantities
 
-  def initialize
-    @product_quantities = Hash.new(0)
-  end
+  def initialize = @product_quantities = Hash.new(0)
 
   def add_item_quantity(product, quantity) = product_quantities[product] += quantity
 
