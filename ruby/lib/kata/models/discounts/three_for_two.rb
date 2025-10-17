25b34781857861d64e9b733a3868b46610fe6ff9
Vahdet Pervan <vpervan22@gmail.com>
2025-10-17 16:00:49 +0200

    Use params instead of ivars

--- ruby/lib/kata/models/discounts/three_for_two.rb
+++ ruby/lib/kata/models/discounts/three_for_two.rb
@@ -2,11 +2,9 @@ module Kata::Discounts
   class ThreeForTwo
     def initialize(options = {})
       @product = options[:product]
-      @unit_price = options[:unit_price]
-      @quantity = options[:quantity]
     end
 
-    def calculate(quantity: nil, unit_price: nil)
+    def calculate(quantity:, unit_price:)
       discount_amount = quantity * unit_price - ((quantity.to_i / 3 * 2 * unit_price) + quantity.to_i % 3 * unit_price)
       Kata::Discount.new(@product, "3 for 2", discount_amount)
     end
