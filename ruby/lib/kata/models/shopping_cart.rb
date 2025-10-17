8ecc41c951884e1ede5e2da836d2266d48594fcd
Vahdet Pervan <vpervan22@gmail.com>
2025-10-17 21:10:56 +0200

    Remove handle_offers method from ShoppingCart.

--- ruby/lib/kata/models/shopping_cart.rb
+++ ruby/lib/kata/models/shopping_cart.rb
@@ -4,13 +4,4 @@ class Kata::ShoppingCart
   def initialize = @product_quantities = Hash.new(0)
 
   def add_item_quantity(product, quantity) = product_quantities[product] += quantity
-
-  def handle_offers(receipt, offers, catalog)
-    @product_quantities.each do |product, quantity|
-      next unless offers.key?(product)
-
-      discount = offers[product].calculate(unit_price: catalog.unit_price(product), quantity:)
-      receipt.add_discount(discount) if discount
-    end
-  end
 end
