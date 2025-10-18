e228f76a35c232c3e13cf9f41c76ee5f43c67d09
Vahdet Pervan <vpervan22@gmail.com>
2025-10-18 15:26:02 +0200

    Use class instead of Struct.

--- ruby/lib/kata/models/receipt_item.rb
+++ ruby/lib/kata/models/receipt_item.rb
@@ -1,5 +1,10 @@
-Kata::ReceiptItem = Struct.new(:product, :quantity, :price, :total_price) do
+class Kata::ReceiptItem
+  attr_reader :product, :quantity, :price, :total_price
 
-  undef :product=, :quantity=, :price=, :total_price=
-
+  def initialize(product, quantity, price, total_price)
+    @product = product
+    @quantity = quantity
+    @price = price
+    @total_price = total_price
+  end
 end
