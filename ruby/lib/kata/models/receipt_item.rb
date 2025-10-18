ca2f79b5cd9eb49ac754a5e2c718fac6009a8f7d
Vahdet Pervan <vpervan22@gmail.com>
2025-10-18 17:32:21 +0200

    From Ruby 3.2, we can use Data.define.

--- ruby/lib/kata/models/receipt_item.rb
+++ ruby/lib/kata/models/receipt_item.rb
@@ -1,10 +1,1 @@
-class Kata::ReceiptItem
-  attr_reader :product, :quantity, :price, :total_price
-
-  def initialize(product, quantity, price, total_price)
-    @product = product
-    @quantity = quantity
-    @price = price
-    @total_price = total_price
-  end
-end
+Kata::ReceiptItem = Data.define(:product, :quantity, :price, :total_price)
