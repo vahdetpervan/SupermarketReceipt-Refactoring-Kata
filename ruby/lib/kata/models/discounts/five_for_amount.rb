5aa6be70ae9f60ba326e87b9a8eaac2fdad3b98f
Vahdet Pervan <vpervan22@gmail.com>
2025-10-24 20:42:16 +0200

    Use base price ivar

--- ruby/lib/kata/models/discounts/five_for_amount.rb
+++ ruby/lib/kata/models/discounts/five_for_amount.rb
@@ -9,8 +9,8 @@ module Kata::Discounts
     def calculate(quantity:, unit_price:)
       return unless quantity.to_i >= 5
 
-      discount_total = unit_price * quantity - (@argument * (quantity.to_i / 5) + quantity.to_i % 5 * unit_price)
-      Kata::Discount.new(@product, 5.to_s + " for " + @argument.to_s, discount_total)
+      discount_total = unit_price * quantity - (@base_price * (quantity.to_i / 5) + quantity.to_i % 5 * unit_price)
+      Kata::Discount.new(@product, 5.to_s + " for " + @base_price.to_s, discount_total)
     end
   end
 end
--- ruby/lib/kata/models/discounts/two_for_amount.rb
+++ ruby/lib/kata/models/discounts/two_for_amount.rb
@@ -7,9 +7,9 @@ module Kata::Discounts
     end
 
     def calculate(quantity:, unit_price:)
-      total = @argument * (quantity.to_i / 2) + quantity.to_i % 2 * unit_price
+      total = @base_price * (quantity.to_i / 2) + quantity.to_i % 2 * unit_price
       discount_n = unit_price * quantity - total
-      Kata::Discount.new(@product, "2 for " + @argument.to_s, discount_n)
+      Kata::Discount.new(@product, "2 for " + @base_price.to_s, discount_n)
     end
   end
 end
