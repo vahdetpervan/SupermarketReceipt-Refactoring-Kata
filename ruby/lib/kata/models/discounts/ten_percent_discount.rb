058f5e2fdebd73a0dded320d80dd3d5abeb7d07c
Vahdet Pervan <vpervan22@gmail.com>
2025-10-26 12:00:09 +0100

    Simplify description

--- ruby/lib/kata/models/discounts/ten_percent_discount.rb
+++ ruby/lib/kata/models/discounts/ten_percent_discount.rb
@@ -6,7 +6,7 @@ module Kata::Discounts
     end
 
     def calculate(quantity:, unit_price:)
-      Kata::Discount.new(@product, @base_price.to_s + "% off", quantity * unit_price * @base_price / 100.0)
+      Kata::Discount.new(@product, "#{@base_price}% off", quantity * unit_price * @base_price / 100.0)
     end
   end
 end
