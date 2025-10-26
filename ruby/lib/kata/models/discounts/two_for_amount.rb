207ae23e479d40ffdba15399f1503550694e658a
Vahdet Pervan <vpervan22@gmail.com>
2025-10-26 12:03:57 +0100

    Simplify description

--- ruby/lib/kata/models/discounts/two_for_amount.rb
+++ ruby/lib/kata/models/discounts/two_for_amount.rb
@@ -8,7 +8,7 @@ module Kata::Discounts
     def calculate(quantity:, unit_price:)
       total = @base_price * (quantity.to_i / 2) + quantity.to_i % 2 * unit_price
       discount_n = unit_price * quantity - total
-      Kata::Discount.new(@product, "2 for " + @base_price.to_s, discount_n)
+      Kata::Discount.new(@product, "2 for #{@base_price}", discount_n)
     end
   end
 end
