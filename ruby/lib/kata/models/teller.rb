dbf54d56dca96d9ca0029775f8920762ea313363
Vahdet Pervan <vpervan22@gmail.com>
2025-10-28 00:00:13 +0100

    Use .select and remove guard clause

--- ruby/lib/kata/models/teller.rb
+++ ruby/lib/kata/models/teller.rb
@@ -42,9 +42,7 @@ class Kata::Teller
   end
 
   def handle_offers(the_cart)
-    the_cart.each do |product, quantity|
-      next unless @offers.key?(product)
-
+    the_cart.select { |product, _quantity| @offers.key?(product) }.each do |product, quantity|
       discount = @offers[product].calculate(unit_price: @catalog.unit_price(product), quantity:)
       @receipt.add_discount(discount) if discount
     end
