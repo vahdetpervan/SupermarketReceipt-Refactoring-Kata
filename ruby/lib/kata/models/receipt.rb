e54787513ebb055922e43c80bb73c00a5992b00d
Vahdet Pervan <vpervan22@gmail.com>
2025-11-06 00:01:21 +0100

    Add @bundles array, change total price to include bundles, and add add_bundle method.

--- ruby/lib/kata/models/receipt.rb
+++ ruby/lib/kata/models/receipt.rb
@@ -1,13 +1,15 @@
 class Kata::Receipt
-  attr_reader :items, :discounts
+  attr_reader :items, :discounts, :bundles
 
   def initialize
-    @items, @discounts = [], []
+    @items, @discounts, @bundles = [], [], []
   end
 
-  def total_price = @items.sum(&:total_price) - @discounts.sum(&:discount_amount)
+  def total_price = @items.sum(&:total_price) - @discounts.sum(&:discount_amount) - @bundles.sum(&:discount_amount).round(2)
 
   def add_product(product, quantity, price, total_price) = @items << Kata::ReceiptItem.new(product, quantity, price, total_price)
 
   def add_discount(discount) = @discounts << discount
+
+  def add_bundle(bundle) = @bundles << bundle
 end
