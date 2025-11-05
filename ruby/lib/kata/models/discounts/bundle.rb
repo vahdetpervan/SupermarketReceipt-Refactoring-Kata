ded36d5d3669474933e8ab1dfcf7ddd6f4ee2acf
Vahdet Pervan <vpervan22@gmail.com>
2025-11-05 20:17:14 +0100

    New bundle class.

---
+++ ruby/lib/kata/models/discounts/bundle.rb
@@ -0,0 +1,17 @@
+module Kata::Discounts
+  class Bundle
+    attr_reader :products
+    def initialize(options = {})
+      @products = options[:products]
+      @catalog = options[:catalog]
+      @discount_amount = options[:discount_amount]
+    end
+
+    def calculate = Kata::Discount.new(@products, "Bundle", product_prices * @discount_amount / 100)
+
+    private
+
+    def product_prices = @catalog.products.values_at(*product_names)
+    def product_names = @products.map(&:name)
+  end
+end
